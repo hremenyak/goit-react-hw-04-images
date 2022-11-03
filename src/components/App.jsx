069cc67a520eaedc99269d91be37801ee6ff7bf4
hremenyak <hremenyak@gmail.com>
2022-11-03 16:46:55 +0100

    initial settings

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <div> Gallery</div>
+        <GlobalStyle />
+      </div>
+    );
+  }
+}
