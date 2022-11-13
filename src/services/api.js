932951a1191b39db1b2a8c8dabe1220432e4c7c1
hremenyak <hremenyak@gmail.com>
2022-11-13 23:07:22 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const KEY = '29943348-171bd2ecaab641b0bb07a2401';
 
 export const fetchImages = async (query, page) => {
   const { data } = await axios.get(
-    `?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=120`
+    `?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
 
   return data;
